43fdf2ffe052d31073046ca2ad36d6145f30919b
Nyasha <nyashabryan23@gmail.com>
2018-04-24 06:15:12 +0100

    Playing around with log in functionality with Gumtree. Removed the log in details from the main.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import requests
 
-r = requests.get('https://www.gumtree.co.za/')
+
+INFO_FILE = "info.txt"
+
+def setinfo(name):
+    with open(INFO_FILE, "r") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if line.split(': ')[0] == name:
+            return line.split(': ')[1][:-1]
+    return "None "
+
+
+USER_NAME = setinfo('USER_NAME')
+PASSWORD =  setinfo('PASSWORD')
+
+TIME_OUT = 1
+
+ 
+r = requests.get('https://www.gumtree.co.za/login.html', timeout = TIME_OUT)
+
+login_details = {"email": USER_NAME, "password": PASSWORD}
+s = requests.post('https://www.gumtree.co.za/login', timeout = TIME_OUT, data = login_details)
+
+print (s.cookies)
+
+
