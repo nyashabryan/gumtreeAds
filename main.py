55bcb6c016c41942c2eb05a71e820ceaefe2b150
Nyasha <nyashabryan23@gmail.com>
2018-04-25 04:40:45 +0100

    Made utility module util.py to deal with functions needed in the main script

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import requests
 
+import util
 
 INFO_FILE = "info.txt"
 
-def setinfo(name):
-    with open(INFO_FILE, "r") as f:
-        lines = f.readlines()
 
-    for line in lines:
-        if line.split(': ')[0] == name:
-            return line.split(': ')[1][:-1]
-    return "None "
-
-
-USER_NAME = setinfo('USER_NAME')
-PASSWORD =  setinfo('PASSWORD')
+USER_NAME = util.setinfo('USER_NAME')
+PASSWORD =  util.setinfo('PASSWORD')
 
 TIME_OUT = 1
 
@@ -25,5 +17,5 @@ login_details = {"email": USER_NAME, "password": PASSWORD}
 s = requests.post('https://www.gumtree.co.za/login', timeout = TIME_OUT, data = login_details)
 
 print (s.cookies)
-
+next = requests
 
---
+++ util.py
@@ -0,0 +1,9 @@
+
+def setinfo(name):
+    with open(INFO_FILE, "r") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if line.split(': ')[0] == name:
+            return line.split(': ')[1][:-1]
+    return "None "
